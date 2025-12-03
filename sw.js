be29a87f7f73b576f03388695a243cad21652937
sergeyaimail <sergeyaimail@gmail.com>
2025-12-03 14:16:43 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+// Service Worker для PWA
+const CACHE_NAME = 'speech-notes-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return new Response('Offline - файл недоступен', { status: 503 });
+    })
+  );
+});
